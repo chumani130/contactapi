5b4bed02d6de4a424115e8189aa639402d6311de
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-28 14:14:56 +0200

    Update ContactResource.java

--- src/main/java/com/chumz/contactapi/resource/ContactResource.java
+++ src/main/java/com/chumz/contactapi/resource/ContactResource.java
@@ -6,8 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.chumz.contactapi.constant.Constant.PHOTO_DIRECTORY;
 
 @RestController
 @RequestMapping("/contacts")
@@ -25,5 +31,21 @@ public class ContactResource {
     public ResponseEntity<Page<Contact>> getContact(@RequestParam(value = "page", defaultValue = "0") int page,
                                                     @RequestParam(value = "size", defaultValue = "10") int size) {
         return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Contact> getContact(@PathVariable(value = "id") String id) {
+        return ResponseEntity.ok().body(contactService.getContact(id));
+    }
+
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id") String id,
+                                              @RequestParam("file")MultipartFile file) {
+        return ResponseEntity.ok().body(contactService.upLoadPhoto(id, file));
+    }
+
+    @GetMapping(path = "/image/{filename}")
+    public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
     }
 }
