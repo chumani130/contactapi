897fca75099dcc9e4215d9bd0f9417c105de989b
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-28 11:45:53 +0200

    Update Contact.java

--- src/main/java/com/chumz/contactapi/domain/Contact.java
+++ src/main/java/com/chumz/contactapi/domain/Contact.java
@@ -1,25 +1,27 @@
 package com.chumz.contactapi.domain;
-//@authur Chumani
-//@version 1.0
-//@license chumz,
-//@email chumanimadikizela@gmail.com
-//@since 27/06/2024
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
 
 @Entity
-@Table(name = "contacts")
 @Getter
 @Setter
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@JsonInclude(NON_DEFAULT)
+@Table(name = "contacts")
 public class Contact {
-
+    @Id
+    @UuidGenerator
+    @Column(name="id", unique = true, updatable = false)
     private String id;
     private String name;
     private String email;
