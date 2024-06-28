642934be7e7594ee12d40b73ecc04089313d3a47
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-06-28 11:59:12 +0200

    Update ContactResource.java

--- src/main/java/com/chumz/contactapi/resource/ContactResource.java
+++ src/main/java/com/chumz/contactapi/resource/ContactResource.java
@@ -1,4 +1,29 @@
 package com.chumz.contactapi.resource;
 
+import com.chumz.contactapi.domain.Contact;
+import com.chumz.contactapi.service.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
 public class ContactResource {
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+        //return ResponseEntity.ok().body(contactService.createContact(contact));
+        return ResponseEntity.created(URI.create("/contacts/<user ID")).body(contactService.createContact(contact));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Contact>> getContact(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                    @RequestParam(value = "size", defaultValue = "10") int size) {
+        return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
+    }
 }
