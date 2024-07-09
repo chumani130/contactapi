66d6d72dece0cba6912eff4a7f0e1b4e45d83e3e
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-09 15:46:15 +0200

    Update ContactResource.java

--- src/main/java/com/chumz/contactapi/resource/ContactResource.java
+++ src/main/java/com/chumz/contactapi/resource/ContactResource.java
@@ -29,7 +29,7 @@ public class ContactResource {
     }
 
     @GetMapping
-    public ResponseEntity<Page<Contact>> getContact(@RequestParam(value = "page", defaultValue = "0") int page,
+    public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue = "0") int page,
                                                     @RequestParam(value = "size", defaultValue = "10") int size) {
         return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
     }
